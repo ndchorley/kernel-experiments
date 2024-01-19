14820a82205f126cc11ba59585c9d95049a909ce
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-01-19 13:59:17 +0000

    Allocate device numbers for simplechar

--- simple-char-driver/simplechar.c
+++ simple-char-driver/simplechar.c
@@ -1,15 +1,30 @@
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/types.h>
 MODULE_LICENSE("Dual BSD/GPL");
+
+dev_t dev;
+const int NUMBER_OF_DEVICES = 1;
 
 static int simple_init(void)
 {
+  int result = alloc_chrdev_region(&dev, 0, NUMBER_OF_DEVICES, "simplechar");
+
+  if (result < 0) {
+    printk(KERN_ALERT "Couldn't allocate device numbers for simplechar\n");
+    return result;
+  }
+
   printk(KERN_ALERT "simplechar loaded\n");
+
   return 0;
 }
 
 static void simple_exit(void)
 {
+  unregister_chrdev_region(dev, NUMBER_OF_DEVICES);
+
   printk(KERN_ALERT "simplechar unloaded\n");
 }
 
