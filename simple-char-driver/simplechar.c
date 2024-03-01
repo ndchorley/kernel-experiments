d2b8abc58b8df98adeef842d72fb9c98c070bd40
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-01 13:32:22 +0000

    Allow simplechar to handle the open system call

--- simple-char-driver/simplechar.c
+++ simple-char-driver/simplechar.c
@@ -1,3 +1,4 @@
+#include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -7,12 +8,36 @@ MODULE_LICENSE("Dual BSD/GPL");
 dev_t dev;
 const int NUMBER_OF_DEVICES = 1;
 
+struct cdev cdev;
+
+static int simplechar_open(struct inode *inode, struct file *file)
+{
+  printk(KERN_ALERT "open called\n");
+
+  return 0;
+}
+
+struct file_operations simple_fops = {
+  .owner = THIS_MODULE,
+  .open = simplechar_open
+};
+
 static int simple_init(void)
 {
   int result = alloc_chrdev_region(&dev, 0, NUMBER_OF_DEVICES, "simplechar");
 
   if (result < 0) {
     printk(KERN_ALERT "Couldn't allocate device numbers for simplechar\n");
+    return result;
+  }
+
+  cdev_init(&cdev, &simple_fops);
+  cdev.owner = THIS_MODULE;
+
+  result = cdev_add(&cdev, dev, 1);
+
+  if (result < 0) {
+    printk(KERN_ALERT "Couldn't register simplechar device\n");
     return result;
   }
 
